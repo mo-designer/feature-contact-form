5d0e9204f2f6970cd4f43bdc957bfa051e020ebe
movegeta <autowago21@gmail.com>
2022-05-10 10:16:31 +0200

    Handler in PHP is ok! It works!

--- handler_contact.php
+++ handler_contact.php
@@ -1,8 +1,12 @@
 <?php
 
+$to      = 'ananidijon21@yahoo.fr';
 $username = strip_tags($_POST['data-username']); 
 $mail = strip_tags($_POST['data-mail']);
 $subject = strip_tags($_POST['data-subject']);
-$message = strip_tags($_POST['data-message']);
-// strip_tags : supprime les balises HTML et PHP d'une chaîne
+$message = $username .": " .strip_tags($_POST['data-message']);
+$headers = 'From: ' . $mail . "\r\n" .
+'Reply-To:' . $mail . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
 
+mail($to, $subject, $message, $headers);
